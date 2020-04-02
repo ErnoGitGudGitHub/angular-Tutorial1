50998d586efe7d3fb30bdab0056991a1db06ad79
ErnoGitGudGitHub <fehererno4444@gmail.com>
2020-04-02 12:40:06 +0200

    3 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,14 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable()
 export class CartService {
   items = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   addToCart(product){
     this.items.push(product);
@@ -17,6 +21,10 @@ export class CartService {
   clearCart(){
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices(){
+    return this.http.get('/assets/shipping.json');
   }
 
 }
